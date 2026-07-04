c324715c0f1a480d5fcb7cdb75f5f605509d6f43
Kingo team <mahdiarpdf@gmail.com>
2026-07-04 23:22:05 +0330

    Add files via upload

---
+++ Android/settings.gradle.kts
@@ -0,0 +1,23 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+        flatDir {
+            dirs("v2ray/libs")
+        }
+    }
+}
+
+rootProject.name = "Kingo vpn"
+include(":app", ":v2ray")
